82d05edac641e5214f1fe326b6a90ec00491251e
Smriti K Vantkar <smritivantkar@gmail.com>
2025-05-05 01:10:45 +0530

    first commit

---
+++ src/db/initDB.ts
@@ -0,0 +1,12 @@
+import { PGlite } from '@electric-sql/pglite';
+
+export const db = new PGlite('idb://patients-db');
+await db.exec(`
+  CREATE TABLE IF NOT EXISTS patients (
+    id SERIAL PRIMARY KEY,
+    name TEXT,
+    age INTEGER,
+    gender TEXT,
+    contact TEXT
+  );
+`);
